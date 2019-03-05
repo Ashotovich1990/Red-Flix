964f0478f5107fd6f3424386a58c5de95fda9f83
Aram Sargsyan <aramsargsyan9902gmail.com>
2019-03-05 14:17:55 -0800

    Fixing heroku

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,14 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+p 'after bundler require'
 module Redflix
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    p 'before confid default'
     config.load_defaults 5.2
+    p 'after config default'
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
